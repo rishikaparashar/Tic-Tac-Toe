188a448e5daa5258bd4bca977284f5b5d2b5dc1f
Rishika Parashar <rishikaparashar15@gmail.com>
2020-08-19 15:23:21 -0500

    Tic Tac Toe app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BoardComponent } from './board.component';
+import {CellComponent} from './cell.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, BoardComponent, CellComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
